56c44fc4c26ec5aaaf41bc853e12a64c1d84162f
Viktor <vikanonymous@hotmail.com>
2020-12-25 20:16:13 +0200

    Web scraper init

---
+++ Learning/WebScraperConsole/Run.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+
+namespace WebScraperConsole
+{
+    class Run
+    {
+        static async Task Main(string[] args)
+        {
+            string url = "https://www.cvonline.lt/darbo-skelbimai/informacines-technologijos";
+
+            List<string> results = await ParseAsync(url);
+            if (results.Count == 0)
+            {
+                Console.WriteLine("No results found.");
+            }
+            else
+            {
+                foreach (var result in results)
+                {
+                    Console.WriteLine(result);
+                }
+            }
+        }
+
+        static async Task<List<string>> ParseAsync(string url)
+        {
+            string responseBody = await HttpResponse(url);
+            HtmlDocument htmlDoc = new HtmlDocument();
+            htmlDoc.LoadHtml(responseBody);
+            List<HtmlNode> links = htmlDoc.DocumentNode.Descendants().Where(node => node.HasClass("offer_primary_info")).ToList();
+            return links.Select(link => link.FirstChild.FirstChild.InnerHtml).ToList();
+        }
+        static async Task<string> HttpResponse(string url)
+        {
+            HttpClient client = new HttpClient();
+            var response = await client.GetAsync(url);
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new Exception();
+            }
+            return await response.Content.ReadAsStringAsync();
+        }
+    }
+}
